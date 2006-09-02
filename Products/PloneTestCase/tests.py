3dbcfedb4bc9fba39d37a8404e53128645ea2efb
Stefan H. Holek <stefan@epy.co.at>
2006-09-02 19:12:01 +0000

    Prettify.
    svn path=/PloneTestCase/trunk/; revision=29768

--- Products/PloneTestCase/tests.py
+++ Products/PloneTestCase/tests.py
@@ -1,21 +1,16 @@
 import unittest
-import os, sys
-
-import os, sys
+import os
+import Products.PloneTestCase
 
 suite = unittest.TestSuite()
 
-if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py')) 
-
 names = os.listdir(os.path.dirname(__file__))
-tests = [x[:-3] for x in names \
-         if x.startswith('test') and x.endswith('.py') \
-         and not x == 'tests.py']
+tests = [x[:-3] for x in names
+         if x.startswith('test') and x.endswith('.py')
+         and x != 'tests.py']
 
-import Products.PloneTestCase
 for test in tests:
-    m = __import__("Products.PloneTestCase.%s" %test)
+    m = __import__('Products.PloneTestCase.%s' % test)
     m = getattr(Products.PloneTestCase, test)
     if hasattr(m, 'test_suite'):
         suite.addTest(m.test_suite())
