c3930acf1d62dbd7640bec580851d64b98e4595a
Stefan H. Holek <stefan@epy.co.at>
2008-04-21 13:48:13 +0000

    Use svn copy to keep old location working (for now).
    svn path=/PloneTestCase/trunk/; revision=63212

---
+++ Products/PloneTestCase/tests.py
@@ -0,0 +1,30 @@
+#
+# Allow testrunners to pick up tests
+#
+
+# $Id$
+
+import unittest
+import os
+import Products.PloneTestCase
+
+suite = unittest.TestSuite()
+
+try:
+    names = os.listdir(os.path.dirname(__file__))
+except OSError:
+    tests = []
+else:
+    tests = [x[:-3] for x in names
+             if x.startswith('test') and x.endswith('.py')
+             and x != 'tests.py']
+
+for test in tests:
+    m = __import__('Products.PloneTestCase.%s' % test)
+    m = getattr(Products.PloneTestCase, test)
+    if hasattr(m, 'test_suite'):
+        suite.addTest(m.test_suite())
+
+def test_suite():
+    return suite
+
---
+++ Products/PloneTestCase/utils.py
@@ -0,0 +1,11 @@
+#
+# Utility functions
+#
+
+# $Id$
+
+from Testing.ZopeTestCase.utils import *
+
+# BBB
+from five import safe_load_site_wrapper
+
