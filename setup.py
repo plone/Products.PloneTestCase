8fd260f62a14b53002a509d36197990f5fa64ec6
Eric Steele <esteele@plone.org>
2011-01-03 21:10:32 +0000

    Bump version.
    svn path=/Products.PloneTestCase/trunk/; revision=229715

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 from setuptools import setup, find_packages
 
-version = '0.9.13'
+version = '0.9.14'
 
 # We only specify our real dependencies when run with Python 2.6.
 # This is to ensure backwards compatibility with older Plone versions which
