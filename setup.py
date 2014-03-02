e5c4e0f5040d1b09a4455d12de2809200e799441
Eric Steele <EricSteele47@gmail.com>
2014-03-02 13:03:37 -0500

    Preparing release 0.9.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 from setuptools import setup, find_packages
 
-version = '0.9.18.dev0'
+version = '0.9.18'
 
 # We only specify our real dependencies when run with Python 2.6.
 # This is to ensure backwards compatibility with older Plone versions which
